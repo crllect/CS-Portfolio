e11e263981424b04b7f9249ed4298cb7b60ae8c7
crllect <crllect@proton.me>
2024-10-23 19:04:29 -0400

    more stuff

--- js/index.js
+++ js/index.js
@@ -22,26 +22,25 @@ document.addEventListener('mousemove', updateMousePosition);
 document.addEventListener('scroll', updateGradientPosition);
 
 async function getLatestCommitHash() {
-  const url = `https://api.github.com/repos/crllect/CS-Portfolio/commits/main`;
+	const url = `https://api.github.com/repos/crllect/CS-Portfolio/commits/main`;
 
-  try {
-    const response = await fetch(url);
+	try {
+		const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} ${response.statusText}`);
-    }
+		if (!response.ok) {
+			throw new Error(`Error: ${response.status} ${response.statusText}`);
+		}
 
-    const data = await response.json();
+		const data = await response.json();
 
-    const latestCommitHash = data.sha;
-    const latestCommitHashStr = document.querySelector('.lastCommitHash')
-    console.log("Latest Commit Hash:", latestCommitHash);
-    latestCommitHashStr.innerHTML = `#${latestCommitHash.substr(0, 7)}`;
-    return latestCommitHash;
-  } catch (error) {
-    console.error("Error fetching the latest commit hash:", error);
-  }
+		const latestCommitHash = data.sha;
+		const latestCommitHashStr = document.querySelector('.lastCommitHash');
+		console.log('Latest Commit Hash:', latestCommitHash);
+		latestCommitHashStr.innerHTML = `#${latestCommitHash.substr(0, 7)}`;
+		return latestCommitHash;
+	} catch (error) {
+		console.error('Error fetching the latest commit hash:', error);
+	}
 }
 
 getLatestCommitHash();
-
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-Hey, this repo is my portfolio for my CS-II class. Thats why its using no framework or anything fancy
+Hey, this repo is my portfolio for my CS-II class. Thats why its using no
+framework or anything fancy
