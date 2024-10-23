68870fe5a49858eca496e1af4fd0fec46e594841
crllect <crllect@proton.me>
2024-10-23 01:23:54 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".btn");
+let mouseX = 0;
+let mouseY = 0;
+
+function updateMousePosition(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  updateGradientPosition();
+}
+
+function updateGradientPosition() {
+  buttons.forEach((btn) => {
+    const rect = btn.getBoundingClientRect();
+    const x = mouseX - rect.left;
+    const y = mouseY - rect.top;
+    btn.style.setProperty("--x", x + "px");
+    btn.style.setProperty("--y", y + "px");
+  });
+}
+
+document.addEventListener("mousemove", updateMousePosition);
+document.addEventListener("scroll", updateGradientPosition);
